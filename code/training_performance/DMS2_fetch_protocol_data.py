9c89e875ceda454b935997e8c295b5615f865fd2
jess-breda <jbreda19@gmail.com>
2023-05-18 16:02:07 -0400

    trials rename

--- code/training_performance/DMS2_fetch_protocol_data.py
+++ code/training_performance/DMS2_fetch_protocol_data.py
@@ -2,68 +2,22 @@ import datajoint as dj
 import numpy as np
 import pandas as pd
 
-import blob_transformation as bt
-
-
-ANIMAL_IDS = ["R610", "R611", "R612", "R613", "R614"]
+import dj_utils as djut
+from dj_utils import ANIMAL_IDS
 
 dj.blob.use_32bit_dims = True  # necessary for pd.blob read
 
 bdata = dj.create_virtual_module("bdata", "bdata")
 
 
-def return_date_window(latest_date=None, n_days_back=None):
-    """
-    Function to create a date window for querying the DataJoint
-    SQL database. Pairs nicely with `fetch_latest_training_data`
-
-    params
-    ------
-    latest_date : str  (optional, default = None)
-        latest date to include in the window, defaults to today
-        if left empty
-    n_days_back : int (optional, default = None)
-        number of days back from `latest_date` to include,
-        defaults to all days if left empty
-
-    Note: if you are out of range of your table (e.g min date)
-    is before the start of training) it's okay.
-
-    returns
-    ------
-    min_date : str
-        minimum date in the specified date window
-    max_date : str
-        maximum date in the specified date window
-
-    example usage
-    ------------
-    `return_date_window(latest_date='2022-11-3', n_days_back=8)`
-    `return_date_window(latest_date=None, n_days_back=7)`
-    """
-
-    if latest_date:
-        date_max = pd.to_datetime(latest_date)
-    else:
-        date_max = pd.Timestamp.today()
-
-    if n_days_back:
-        date_min = date_max - pd.Timedelta(days=n_days_back)
-        date_min = date_min.strftime("%Y-%m-%d")
-    else:
-        date_min = n_days_back  # none
-
-    return date_min, date_max.strftime("%Y-%m-%d")
-
-
-def fetch_latest_training_data(
+def fetch_latest_trials_data(
     animal_ids=None, date_min="2000-01-01", date_max="2030-01-01", verbose=False
 ):
     """
     TODO
     """
-    # TODO add in date range to fetch
-    # TODO maybe add save out here if read in is slow
+    # TODO maybe add save out here if read in is
+
     animal_ids = ANIMAL_IDS if animal_ids is None else animal_ids
     assert type(animal_ids) == list, "animal ids must be in a list"
 
@@ -169,7 +123,7 @@ def convert_to_dicts(blobs):
     dicts = []
 
     for session_blob in blobs:
-        sess_dict = bt.transform_blob(session_blob[data_type])
+        sess_dict = djut.transform_blob(session_blob[data_type])
         dicts.append(sess_dict)
 
     return dicts
@@ -183,7 +137,7 @@ def convert_to_dfs(dicts, sess_ids):
 
     for session_dict in dicts:
         try:
-            dfs.append(bt.blob_dict_to_df(session_dict))
+            dfs.append(djut.blob_dict_to_df(session_dict))
         except:
             print(f"error in fetching df for session {sess_ids}, skipping it")
             pass
