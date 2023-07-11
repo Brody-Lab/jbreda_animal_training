5f5053a2e469299b99ccac1921c7b80aee102e67
jess-breda <jbreda19@gmail.com>
2023-07-11 08:27:36 -0400

    time to spoke plots

--- code/training_performance/plot_trials_info.py
+++ code/training_performance/plot_trials_info.py
@@ -203,7 +203,7 @@ def plot_npokes(trials_df, ax, title="", legend=True):
         x="trial",
         y="n_pokes",
         # note this could throw an error if no pokes happened in a port
-        palette=pu.get_poke_colors(pd.Series(["l", "c", "r"])),
+        palette=pu.get_side_colors(pd.Series(["l", "c", "r"])),
         hue="poke_side",
         ax=ax,
     )
@@ -247,7 +247,7 @@ def plot_npokes_summary(trials_df, ax, title=""):
         data=pokes_df,
         x="poke_side",
         y="n_pokes",
-        palette=pu.get_poke_colors(pd.Series(sides)),
+        palette=pu.get_side_colors(pd.Series(sides)),
         hue="poke_side",
         size=4,
         ax=ax,
@@ -269,5 +269,172 @@ def plot_npokes_summary(trials_df, ax, title=""):
     # aesthetics
     ax.set_xticklabels(sides)
     ax.set(ylabel="N pokes", xlabel="", title=title)
+
+    return None
+
+
+def plot_time_to_first_spoke(trials_df, ax, title="", legend=False):
+    """
+    plot time to first spoke across trials for a single day, this
+    plot only shows the first spoke for each trial- not the first
+    spoke for each side, for each trial.
+
+    params
+    ------
+    trials_df : DataFrame
+        trials dataframe with columns `trial`, `min_time_to_spoke`,
+        `first_spoke` with trials as row index
+    ax : matplotlib.axes
+        axis to plot to
+    title : str, (default = "")
+        title of plot
+    legend : bool, (default = False)
+        whether to include legend or not
+    """
+    sns.scatterplot(
+        data=trials_df,
+        x="trial",
+        y="min_time_to_spoke",
+        ax=ax,
+        hue="first_spoke",
+        palette=pu.get_side_colors(trials_df.first_spoke),
+    )
+
+    # # mark
+    # was_no_answer = df["result"].apply(lambda row: True if row == 6.0 else np.nan)
+    # ax.scatter(x=trials_df.trial, y=was_no_answer, marker="s", color="black")
+
+    # aesthetics
+    upper_lim = trials_df["min_time_to_spoke"].quantile(0.99)  # exclude outliers
+    ax.set_ylim(0.1, upper_lim)
+    _ = ax.set(ylabel="1st Spoke Time [s]", xlabel="Trials", title=title)
+    pu.set_legend(ax, legend)
+
+
+def plot_first_spokes_summary_by_correct_side_and_location(
+    trials_df, ax, title="", legend=False
+):
+    """
+    plot first spoke summary by correct side for a day.
+    time_to_first_spoke ~ correct_side + poke_location.
+    Note this plot does show the first spoke for each side,
+    for each trial. So on a left trial, if the mouse poked left first,
+    then right, the first spoke for left will be plotted under the
+    "correct" side in green and the first spoke for right will be
+    plotted under the "incorrect" side, colored in red
+
+    params
+    ------
+    trials_df : DataFrame
+        trials dataframe with columns `trial`, `first_spoke`,
+        `result`, `sides` `first_lpoke`,  `first_rpoke` with
+        trials as row index
+    ax : matplotlib.axes
+        axis to plot to
+    title : str, (default = "")
+        title of plot
+    legend : bool, (default = False)
+        whether to include legend or not
+    """
+
+    # make long df for first l/r poke on each trial
+    first_spoke_df = pd.melt(
+        trials_df,
+        id_vars=["trial", "first_spoke", "result", "sides"],
+        value_vars=["first_lpoke", "first_rpoke"],
+        var_name="poke_side",
+        value_name="poke_time",
+    )
+
+    # determine if poke was on correct side
+    first_spoke_df["correct_side"] = first_spoke_df.sides == first_spoke_df.first_spoke
+
+    # rename for plotting
+    first_spoke_df["poke_side"] = first_spoke_df.poke_side.str.replace(
+        "first_", ""
+    ).str.replace("poke", "")
+
+    sns.stripplot(
+        data=first_spoke_df,
+        x="correct_side",
+        y="poke_time",
+        hue="poke_side",
+        palette=pu.get_side_colors(first_spoke_df.poke_side),
+        ax=ax,
+        dodge=True,
+        alpha=0.5,
+        jitter=0.2,
+    )
+
+    sns.boxplot(
+        data=first_spoke_df,
+        x="correct_side",
+        y="poke_time",
+        hue="poke_side",
+        showfliers=False,
+        dodge=True,
+        width=0.8,
+        linewidth=1,
+        boxprops={"facecolor": "None"},
+        ax=ax,
+    )
+
+    # aesthetics
+    upper_lim = first_spoke_df["poke_time"].quantile(0.99)  # exclude outliers
+    ax.set_ylim(0.1, upper_lim)
+    ax.set(ylabel="Spoke Time [s]", xlabel="Correct")
+    pu.set_legend(ax, legend)
+
+
+def plot_first_spoke_summary_by_location_and_result(
+    trials_df, ax, title="", legend=False
+):
+    """
+    plot first spoke summary given the location of the poke and
+    the result of the trial. Note this only looks at the first
+    spoke in a trial, not for each side, for each trial.
+
+    params
+    ------
+    trials_df : DataFrame
+        trials dataframe with columns `trial`, `min_time_to_spoke`,
+        `result`, `first_spoke` with trials as row index
+    ax : matplotlib axis
+        axis to plot on
+    title : str, (default = "")
+        title of plot
+    legend : bool, (default = False)
+        whether to include legend or not
+    """
+    sns.stripplot(
+        data=trials_df,
+        x="first_spoke",
+        y="min_time_to_spoke",
+        hue="result",
+        hue_order=pu.get_result_order(trials_df.result),
+        palette=pu.get_result_colors(trials_df.result),
+        ax=ax,
+        dodge=True,
+        alpha=0.5,
+        jitter=0.2,
+    )
+    sns.boxplot(
+        data=trials_df,
+        x="first_spoke",
+        y="min_time_to_spoke",
+        hue="result",
+        showfliers=False,
+        dodge=True,
+        width=0.8,
+        linewidth=1,
+        boxprops={"facecolor": "None"},
+        ax=ax,
+    )
+
+    # aesthetics
+    upper_lim = trials_df["min_time_to_spoke"].quantile(0.99)  # exclude outliers
+    ax.set_ylim(0.1, upper_lim)
+    ax.set(title=title, xlabel="1st Spoke", ylabel="Time [s]")
+    pu.set_legend(ax, legend)
 
     return None
