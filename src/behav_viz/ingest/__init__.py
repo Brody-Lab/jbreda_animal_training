193de8a25a4bf8689e8b23ab5a07ca61d6833ef9
jess-breda <jbreda19@gmail.com>
2024-08-23 17:38:58 -0400

    adding in date drop option

--- src/behav_viz/ingest/__init__.py
+++ src/behav_viz/ingest/__init__.py
@@ -2,3 +2,13 @@
 
 from ..utils import dir_utils, dj_utils
 from . import create_days_df, create_trials_df
+
+
+def drop_dates(df, date_drop_dict):
+
+    for animal_id, date in date_drop_dict.items():
+        print("dropping", animal_id, date, "length:", len(df))
+        df = df.query("not (animal_id == @animal_id and date == @date)")
+        print("length:", len(df))
+
+    return df
