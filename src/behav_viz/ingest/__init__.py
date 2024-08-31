941845eeb6d4677710e0249caab845606e3b861f
jess-breda <jbreda19@gmail.com>
2024-08-31 13:04:48 -0400

    updated to have multiple dates per animal

--- src/behav_viz/ingest/__init__.py
+++ src/behav_viz/ingest/__init__.py
@@ -6,9 +6,14 @@ from . import create_days_df, create_trials_df
 
 def drop_dates(df, date_drop_dict):
 
-    for animal_id, date in date_drop_dict.items():
-        print("dropping", animal_id, date, "length:", len(df))
-        df = df.query("not (animal_id == @animal_id and date == @date)")
-        print("length:", len(df))
+    for animal_id, dates in date_drop_dict.items():
+        # Ensure 'dates' is iterable, whether it's a single date or a list of dates
+        if not isinstance(dates, list):
+            dates = [dates]
+
+        for date in dates:
+            print("dropping", animal_id, date, "length:", len(df))
+            df = df.query("not (animal_id == @animal_id and date == @date)")
+            print("length:", len(df))
 
     return df
