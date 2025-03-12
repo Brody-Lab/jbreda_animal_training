201442b930a2b78641845333e9c59e4b8a28151b
jess-breda <jbreda19@gmail.com>
2025-03-12 15:22:38 -0600

    dropping R044

--- src/behav_viz/ingest/__init__.py
+++ src/behav_viz/ingest/__init__.py
@@ -2,6 +2,7 @@
 
 from ..utils import dir_utils, dj_utils
 from . import create_days_df, create_trials_df
+import pandas as pd
 
 
 def drop_dates(df, date_drop_dict):
@@ -15,5 +16,9 @@ def drop_dates(df, date_drop_dict):
             print("dropping", animal_id, date, "length:", len(df))
             df = df.query("not (animal_id == @animal_id and date == @date)")
             print("length:", len(df))
+
+    # this animal had a rig break and needs to be dropped from that point forward
+    r044_max_date = pd.Timestamp("2024-08-18").date()
+    df = df.query("not (animal_id == 'R044' and date > @r044_max_date)")
 
     return df
